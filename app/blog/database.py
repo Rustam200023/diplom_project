298c8ab7abe0cc520937ac0a8c3cc3143a493fff
Rustam200023 <routpourt@gmail.com>
2024-05-04 16:53:06 +0500

    init

--- app/blog/database.py
+++ app/blog/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:rustam2000@localhost/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
